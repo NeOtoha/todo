9239bdbed233ba8b10d4f8b2d8d3c0ffa2a6b3d5
NeOtoha <neo0818neo@yahoo.co.jp>
2020-01-07 11:45:40 +0000

    initial commit

--- app.rb
+++ app.rb
@@ -14,7 +14,22 @@ helpers do
   end
 end
 
+before'/tasks'do
+  if current_user.nil?
+    redirect'/'
+  end
+end
+
+
 get '/' do
+  @list = List.all
+  if current_user.nil?
+    @tasks = Task.none
+  elsif params[:list].nil? then
+    @tasks = current_user.tasks
+  else
+    @tasks = List.find(params[:list]).task.had_by(current_user)
+  end
   erb :index
 end
 
@@ -55,4 +70,69 @@ end
 
 get'/tasks/new' do
   erb :new
+end
+
+post '/tasks' do
+  date = params[:due_date].split('-')
+  list = List.find(params[:list])
+  if Date.valid_date?(date[0].to_i, date[1].to_i, date[2].to_i)
+  current_user.tasks.create(title: params[:title],
+    due_date: Date.parse(params[:due_date]), list_id: list.id)
+  redirect '/'
+else
+  redirect'/tasks/new'
+end
+end
+
+post '/tasks/:id/done' do
+  task = Task.find(params[:id])
+  task.completed = true
+  task.save
+  redirect '/'
+end
+
+get '/tasks/:id/star' do
+  task = Task.find(params[:id])
+  task.star = !task.star
+  task.save
+  redirect '/'
+end
+
+post '/tasks/:id/delete' do
+  task = Task.find(params[:id])
+  task.destroy
+  redirect '/'
+end
+
+get'/tasks/:id/edit'do
+  @task = Task.find(params[:id])
+  erb :edit
+end
+
+post'/tasks/:id'do
+  task = Task.find(params[:id])
+  list = List.find(params[:title])
+  date = params[:due_date].split('-')
+
+  if Date.valid_date?(date[0].to_i,date[1].to_i,date[2].to_i)
+    task.title = CGI.escapeHTML(params[:title])
+    task.due_date = Date.parse(params[:due_date])
+    task.list_id = list.id
+    task.save
+    redirect '/'
+  else
+    redirect "/tasks/#{task.id}/edit"
+  end
+end
+
+get'/tasks/over'do
+ @list = List.all
+ @tasks = current_user.tasks.where('due_date < ?',Date.today).where(completed: [nil,false])
+ erb :index
+end
+
+get'/tasks/done'do
+ @lists = List.all
+ @tasks = current_user.tasks.due_over
+ erb :index
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_101321) do
+ActiveRecord::Schema.define(version: 2020_01_07_072218) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.integer "user_id"
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed"
+    t.date "due_date"
+    t.boolean "star"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
